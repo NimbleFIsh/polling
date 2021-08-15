25bd96b16317653c34a13071205b072625b0c7b9
NimbleFIsh <karpov2001ura@mail.ru>
2021-08-15 17:59:00 +0300

    complete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import { ajax } from 'rxjs/ajax';
 
 function renderMsg(msgAll) {
     const msg_container = document.getElementById('msg_container');
+    const time = new Date(msgAll.timestamp);
+    document.getElementById('lastUpdated').innerText = `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`;
+    document.getElementById('added').innerText = msgAll.messages.length;
     msgAll.messages.forEach(msg => {
         const date = new Date(msg.received);
         msg_container.insertAdjacentHTML('afterbegin', `
@@ -13,14 +16,19 @@ function renderMsg(msgAll) {
             </li>
         `);
     });
+    document.getElementById('total').innerText = msg_container.childElementCount;
 }
 
-interval(3000).subscribe({
-    next: () => {
-        ajax.getJSON('http://localhost:3035/messages/unread')
-        .subscribe(
-            res => renderMsg(res),
-            err => console.error(err)
-        )
-    }
+function ajaxReq() {
+    ajax.getJSON('http://localhost:3035/messages/unread')
+    .subscribe(
+        res => renderMsg(res),
+        err => console.error(err)
+    )
+}
+
+ajaxReq();
+
+interval(5000).subscribe({
+    next: ajaxReq
 });
